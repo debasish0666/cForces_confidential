ea984e6e3460deb04111f4c4a8d3d2a6b0670c34
debasish0666 <sahoobikun12345@gmail.com>
2026-02-28 09:41:55 +0530

    Solved_Target_800level

---
+++ D1cForces_Target_Practice.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        vector<string> grid(10);
+        for (int i = 0; i < 10; i++) {
+            cin >> grid[i];
+        }
+
+        int score = 0;
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (grid[i][j] == 'X') {
+                    int ring = min({i, j, 9 - i, 9 - j});
+                    score += ring + 1;
+                }
+            }
+        }
+
+        cout << score << endl;
+    }
+
+    return 0;
+}
