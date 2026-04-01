5646e716e11c356ec5faaeecb4e7bde5d4689621
debasish0666 <sahoobikun12345@gmail.com>
2026-04-01 07:22:55 +0530

    Solved900_ChessK_BruteTricky

---
+++ D13cForcesForked.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[4] = {-1, 1, -1, 1};
+int dy[4] = {-1, -1, 1, 1};
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long a, b;
+        cin >> a >> b;
+
+        long long x_king, y_king;
+        cin >> x_king >> y_king;
+
+        long long x_queen, y_queen;
+        cin >> x_queen >> y_queen;
+
+        // sets must be inside loop
+        set<pair<long long, long long>> king_hits, queen_hits;
+
+        for (int i = 0; i < 4; i++) {
+            // king moves
+            king_hits.insert({x_king + dx[i] * a, y_king + dy[i] * b});
+            king_hits.insert({x_king + dx[i] * b, y_king + dy[i] * a});
+
+            // queen moves
+            queen_hits.insert({x_queen + dx[i] * a, y_queen + dy[i] * b});
+            queen_hits.insert({x_queen + dx[i] * b, y_queen + dy[i] * a});
+        }
+
+        int ans = 0;
+
+        for (auto position : king_hits) {
+            if (queen_hits.find(position) != queen_hits.end()) {
+                ans++;
+            }
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
