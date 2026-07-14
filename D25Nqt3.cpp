81f261f1dc0958076e18ee60598b07a88762ce8a
debasish0666 <sahoobikun12345@gmail.com>
2026-07-14 05:34:18 +0530

    SolvedcinIgnore

--- D25Nqt3.cpp
+++ D25Nqt3.cpp
@@ -1,34 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int shift;
-    cin>>shift;
-    if(shift<0){
-        cout<<"INVALID INPUT"<<endl;
-    }
-    if (shift>=26){
-        shift%=26;
-    }
-    string s;
-    string ans="";
-    cin>>s;
-    for(int i=0;i<s.size();i++){
-        if(islower(s[i])){
-            ans+=char(s[i] -'a' + shift)%26 + 'a';
-        }
-        else if(isupper(s[i])){
-            ans+=char(s[i] -'A' + shift)%26 + 'A';
-        }
-        else if(isdigit(s[i])){
-            ans += char((s[i]-'0'+shift)%10)+'0';
-        }
-        else if (s[i]==' '){
-            ans+=s[i];
-        }
-        else{
-            ans+=s[i];
-        }
 
+int main() {
+    int shift;
+    cin >> shift;
+
+    if (shift < 0) {
+        cout << "INVALID INPUT";
+        return 0;
     }
-    cout<<ans<<endl;
+
+    shift = shift % 26;
+
+    cin.ignore();          // Remove newline after reading shift
+    string s;
+    getline(cin, s);       // Read the entire line including spaces
+
+    for (int i = 0; i < s.size(); i++) {
+
+        if (islower(s[i])) {
+            s[i] = (s[i] - 'a' + shift) % 26 + 'a';
+        }
+        else if (isupper(s[i])) {
+            s[i] = (s[i] - 'A' + shift) % 26 + 'A';
+        }
+        else if (isdigit(s[i])) {
+            s[i] = (s[i] - '0' + shift) % 10 + '0';
+        }
+        // Spaces and special characters remain unchanged
+    }
+
+    cout << s;
+
+    return 0;
 }
